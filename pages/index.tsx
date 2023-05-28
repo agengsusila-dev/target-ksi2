46bcbce4effa91cecb2d5439138c7f95dc61036b
Geng Susidev <134186100+agengsusila-dev@users.noreply.github.com>
2023-05-28 14:25:12 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,13 @@ export default function Web() {
   return (
     <>
       <Head>
-        <title>Next.js Enterprise Boilerplate</title>
+        <title>Tes</title>
       </Head>
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
             <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
-              Next.js Enterprise Boilerplate
+              HALO
             </h1>
             <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
               Jumpstart your enterprise project with our feature-packed, high-performance Next.js boilerplate!
